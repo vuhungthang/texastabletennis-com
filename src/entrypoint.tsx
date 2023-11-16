a2d46c633aede23c0fdf7500aaec0cd51ad8318a
Thang Vu <vuhungthang2707@gmail.com>
2023-11-16 01:34:50 +0000

    adding content for the website with typescript and react

---
+++ src/entrypoint.tsx
@@ -0,0 +1,12 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+
+const rootElement = document.getElementById("root");
+
+// New as of React v18.x
+const root = createRoot(rootElement!);
+
+root.render(
+  <App />
+);
