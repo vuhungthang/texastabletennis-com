fe01374e06ea531ca01e0501e95a4667a7b712bd
Matt Lo <mattlo.developer@gmail.com>
2023-11-20 22:07:16 -0600

    added emotion dependency for CSS support, added test styles, added test content

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {appStyle} from './AppStyles';
 
 export default function App() {
   return (
-    <div>
-      Blablaba
+    <div className={appStyle}>
+      <p>
+        Test 1
+      </p>
+
+      <div className="testing">
+        Test 2
+      </div>
     </div>
   )
 }
---
+++ src/components/App/AppStyles.tsx
@@ -0,0 +1,15 @@
+import {css} from '@emotion/css';
+
+export const appStyle = css`
+  p {
+    font-size: 14px;
+    color: red;
+  }
+  
+  .testing {
+    padding: 15px;
+    margin: 15px;
+    border-radius: 3px;
+    border: solid 1px blue;
+  }
+`;
